94daf4a99d9a97b42e0089990adc4b20ec1d6af1
Sivashankar2122 <sivashankargg22@gamil.com>
2023-06-03 22:53:22 +0530

    commit

--- lib/responsive_any.dart
+++ lib/responsive_any.dart
@@ -1,3 +1,19 @@
 library responsive_any;
 
+import 'package:flutter/material.dart';
+
 export 'responsive/responsive.dart';
+
+class First extends StatefulWidget {
+  const First({Key? key}) : super(key: key);
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold();
+  }
+}
